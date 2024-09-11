d45e537711313502116f9dabf9e9a986dbbe6c85
Jihyun-Choi <034179@naver.com>
2024-09-11 23:58:34 +0900

    feat: authentication app 추가 및 jwt 설정 추가

--- auth_server/auth_src/config/urls.py
+++ auth_server/auth_src/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -42,6 +42,7 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("auth/", include("authentication.urls")),
     path("admin/", admin.site.urls),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
