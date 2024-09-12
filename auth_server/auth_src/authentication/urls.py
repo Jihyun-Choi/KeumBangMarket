e34add6b5668cfa881e9910656c07b791d66a454
Jihyun-Choi <034179@naver.com>
2024-09-12 22:57:31 +0900

    feat: RegisterAPIView 구현

--- auth_server/auth_src/authentication/urls.py
+++ auth_server/auth_src/authentication/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-
+from authentication.views import LogoutAPIView, LoginAPIView, RegisterAPIView
 
 app_name = "authentication"
 router = DefaultRouter()
 
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("register/", RegisterAPIView.as_view(), name="register"),
 ]
 
 urlpatterns += router.urls
--- auth_server/auth_src/authentication/views/__init__.py
+++ auth_server/auth_src/authentication/views/__init__.py
@@ -1,2 +1,3 @@
 from .login_api_view import LoginAPIView
 from .logout_api_view import LogoutAPIView
+from .register_api_view import RegisterAPIView
