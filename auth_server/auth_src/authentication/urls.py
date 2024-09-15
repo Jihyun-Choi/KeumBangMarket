996a8db42c335236633c937317ecbccc52276ddb
Jihyun-Choi <034179@naver.com>
2024-09-15 23:00:59 +0900

    feat: 토큰 재발급 API

--- auth_server/auth_src/authentication/urls.py
+++ auth_server/auth_src/authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from authentication.views import LogoutAPIView, LoginAPIView, RegisterAPIView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 app_name = "authentication"
 router = DefaultRouter()
@@ -9,6 +11,7 @@ urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
     path("register/", RegisterAPIView.as_view(), name="register"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 urlpatterns += router.urls
